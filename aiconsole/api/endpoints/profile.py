99a074081c056d959f462b86fc656a15d99d49af
Maciej Cielecki <cielecki@gmail.com>
2023-10-10 14:45:06 +0200

    Update profile.py

--- aiconsole/api/endpoints/profile.py
+++ aiconsole/api/endpoints/profile.py
@@ -16,8 +16,8 @@ async def profile_image(image: str):
         return FileResponse(os.path.join(AGENTS_DIRECTORY, image))
     
     with resources.path(AGENTS_CORE_RESOURCE, image) as static_path:
-        if os.path.exists(str(static_path)):
-            return FileResponse(str(static_path))
+        if os.path.exists(static_path):
+            return FileResponse(static_path)
     
     with resources.path(AGENTS_CORE_RESOURCE, 'default.jpg') as static_path:
         return FileResponse(static_path)
