fa1918eb6e83f7c5fd073bc14c1911369975fcff
piotrkrych <piotr.krych.ext@understand.ai>
2023-10-12 15:50:52 +0200

    change in exception message

--- aiconsole/gpt/exceptions.py
+++ aiconsole/gpt/exceptions.py
@@ -1,6 +1,6 @@
 class NoOpenAPIKeyException(Exception):
     def __str__(self):
-        return "No API key provided. " \
+        return "No API key provided or is invalid. " \
                "You must set the environment variable OPENAI_API_KEY=<API-KEY>. " \
                "Generate API keys in the OpenAI web interface. " \
                "See https://platform.openai.com/account/api-keys for details."
