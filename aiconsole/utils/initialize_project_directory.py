5a9f1cfcf1dda0c310136ec02091c340bafb5f49
piotrkrych <piotr.krych.ext@understand.ai>
2023-10-13 16:47:06 +0200

    fixes

--- aiconsole/utils/initialize_project_directory.py
+++ aiconsole/utils/initialize_project_directory.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 _log = logging.getLogger(__name__)
 
+
 def initialize_project_directory():
     # if .aic does not exist in cwd, copy the entire contents of template to cwd
-    print('initialize_project_directory ' * 20)
-    if False and not os.path.exists(".aic"):
-        print("Initializing project directory ...")
+    if not os.path.exists(".aic"):
+        _log.info("Initializing project directory ...")
         copy_tree(Path("./aiconsole/template"), Path("."))
     else:
-        print("Project directory already initialized")
+        _log.info("Project directory already initialized")
 
 
 def copy_tree(src: Path, dst: Path):
