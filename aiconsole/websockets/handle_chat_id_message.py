40db70872c1213dc3ffc3d25711c56541e1aef1d
Maciej Cielecki <cielecki@gmail.com>
2023-10-17 10:05:52 +0200

    Removed notification

--- aiconsole/websockets/handle_chat_id_message.py
+++ aiconsole/websockets/handle_chat_id_message.py
@@ -1,11 +1,6 @@
 from aiconsole.websockets.connection_manager import AICConnection
 from aiconsole.websockets.incoming_messages import SetChatIdWSMessage
-from aiconsole.websockets.outgoing_messages import NotificationWSMessage
 
 
 async def handle_chat_id_message(connection: AICConnection, message: SetChatIdWSMessage):
     connection.chat_id = message.chat_id
-    await connection.send(NotificationWSMessage(
-        title="Chat ID Set",
-        message=f"Chat ID set to {message.chat_id}"
-    ))
