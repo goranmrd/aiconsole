af9f9792cece9da16e4e9603f7910fa58c39b3cb
Maciej Cielecki <cielecki@gmail.com>
2023-10-25 00:55:36 +0200

    Cleanup of preview, but still the styles do not match

--- web/src/components/materials/MarkdownPreview.tsx
+++ web/src/components/materials/MarkdownPreview.tsx
@@ -1,39 +1,12 @@
 import ReactMarkdown from 'react-markdown';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight';
-
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
-import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
-import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
-import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown';
-import python from 'react-syntax-highlighter/dist/cjs/languages/prism/python';
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
-import applescript from 'react-syntax-highlighter/dist/cjs/languages/prism/applescript';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { cn } from '@/utils/styles';
-
-SyntaxHighlighter.registerLanguage('tsx', tsx);
-SyntaxHighlighter.registerLanguage('typescript', typescript);
-SyntaxHighlighter.registerLanguage('scss', scss);
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('markdown', markdown);
-SyntaxHighlighter.registerLanguage('json', json);
-SyntaxHighlighter.registerLanguage('python', python);
-SyntaxHighlighter.registerLanguage('applescript', applescript);
 
 interface MarkdownPreviewProps {
   text: string;
-  disabled?: boolean;
+  disabled?: boolean; 
 }
-
-const customDoccoStyle = {
-  ...dracula,
-  'code[class*="language-"]': {
-    backgroundColor: 'transparent',
-  },
-};
 
 const customDivStyle = {
   backgroundColor: 'transparent',
@@ -51,16 +24,15 @@ const MarkdownPreview = ({ text, disabled }: MarkdownPreviewProps) => {
           })}
         >
           <ReactMarkdown
-            remarkPlugins={[remarkGfm, rehypeHighlight]}
             components={{
               code({ className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '');
 
                 return match ? (
                   <SyntaxHighlighter
-                    style={customDoccoStyle}
+                    style={vscDarkPlus}
                     PreTag="div"
-                    language={match[1]}
+                    language={'markdown'}
                     children={String(children).replace(/\n$/, '')}
                     customStyle={customDivStyle}
                   />
