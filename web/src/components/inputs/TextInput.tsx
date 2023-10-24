e64918611e0d4bad0419f3eb062ccd320bb0feed
Maciej Topor <maciej.topor@10clouds.com>
2023-10-24 15:12:13 +0200

    feat: added validation to SimpleInput

--- web/src/components/inputs/TextInput.tsx
+++ web/src/components/inputs/TextInput.tsx
@@ -1,14 +1,23 @@
 import { ChangeEvent } from 'react';
-
 import { cn } from '@/utils/styles';
+
+const REQUIRED_ERROR_MESSAGE = 'This field is required.';
+
+export type ErrorObject = {
+  [key: string]: string | null;
+};
 
 interface SimpleInputProps {
   label: string;
   value: string;
+  name: string;
   className?: string;
   onChange: (value: string) => void;
   placeholder?: string;
   disabled?: boolean;
+  required?: boolean;
+  errors?: ErrorObject;
+  setErrors?: React.Dispatch<React.SetStateAction<ErrorObject>>;
 }
 
 export function SimpleInput({
@@ -18,9 +27,38 @@ export function SimpleInput({
   onChange,
   placeholder,
   disabled = false,
+  required,
+  name,
+  errors,
+  setErrors,
 }: SimpleInputProps) {
+  const checkIfEmpty = (value: string) => {
+    if (required && value.trim() === '') {
+      setErrors?.((prevErrors) => ({
+        ...prevErrors,
+        ...{ [name]: REQUIRED_ERROR_MESSAGE },
+      }));
+    } else {
+      setErrors?.((prevErrors) => ({
+        ...prevErrors,
+        ...{ [name]: null },
+      }));
+    }
+  };
+
+  const error = errors?.[name];
+
+  const handleBlur = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    checkIfEmpty(e.target.value);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
+    checkIfEmpty(e.target.value);
+  };
+
   return (
-    <>
+    <div className="relative">
       <label htmlFor={label} className="font-bold">
         {label}:
       </label>
@@ -29,17 +67,20 @@ export function SimpleInput({
         placeholder={placeholder}
         id={label}
         value={value}
-        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-          onChange(e.target.value);
-        }}
+        onBlur={handleBlur}
+        onChange={handleChange}
         className={cn(
           className,
-          'resize-none flex-none h-10 bg-black/20 appearance-none border border-transparent rounded w-full py-2 px-3 leading-tight placeholder-gray-400 focus:outline-none focus:border-primary/50 focus:shadow-outline',
+          'resize-none flex-none h-10 bg-black/20 appearance-none border border-transparent rounded w-full py-2 px-3 leading-tight placeholder-gray-400 focus:outline-none focus:border-primary/50 focus:shadow-outline mt-1',
           {
             'opacity-[0.7] cursor-not-allowed': disabled,
+            'border-red-700 focus:border-red-700': error,
           },
         )}
       ></textarea>
-    </>
+      {error && (
+        <div className="text-red-700 text-sm absolute right-0">{error}</div>
+      )}
+    </div>
   );
 }
--- web/src/components/pages/MaterialPage.tsx
+++ web/src/components/pages/MaterialPage.tsx
@@ -13,7 +13,7 @@ import {
 } from '@/types/types';
 import { TopBar } from '@/components/TopBar';
 import { EnumInput } from '@/components/inputs/EnumInput';
-import { SimpleInput } from '@/components/inputs/TextInput';
+import { ErrorObject, SimpleInput } from '@/components/inputs/TextInput';
 import { CodeInput } from '@/components/inputs/CodeInput';
 import { useAICStore } from '@/store/AICStore';
 import { EyeIcon } from '@heroicons/react/24/outline';
@@ -37,6 +37,7 @@ export function MaterialPage() {
   const [preview, setPreview] = useState<RenderedMaterial | undefined>(
     undefined,
   );
+  const [errors, setErrors] = useState<ErrorObject>({});
 
   const isMaterialChanged = () => {
     if (material && materialInitial) {
@@ -47,6 +48,8 @@ export function MaterialPage() {
       return Boolean(!changedFields.length);
     }
   };
+
+  const isError = Object.values(errors).some((error) => error !== null);
 
   const deleteMaterial = useAICStore((state) => state.deleteMaterial);
 
@@ -127,7 +130,7 @@ export function MaterialPage() {
     ? '```md\n' + preview?.content.split('\\n').join('\n') + '\n```'
     : 'Generating preview...';
 
-  const disableSubmit = isMaterialChanged() && !isDuplicate;
+  const disableSubmit = (isMaterialChanged() && !isDuplicate) || isError;
 
   return (
     <div className="App flex flex-col h-screen fixed top-0 left-0 bottom-0 right-0 bg-gray-800/95 text-stone-400 ">
@@ -152,10 +155,15 @@ export function MaterialPage() {
               placeholder=""
               value={material.name}
               disabled={readOnly}
+              required
+              name="material_name"
+              errors={errors}
+              setErrors={setErrors}
               onChange={(value) => setMaterial({ ...material, name: value })}
             />
             <SimpleInput
               label="Usage"
+              name="usage"
               value={material.usage}
               disabled={readOnly}
               onChange={(value) => setMaterial({ ...material, usage: value })}
