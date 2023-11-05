4d705072df5865eb19431f1b80e559aa3dccbc9e
Maciej Cielecki <cielecki@gmail.com>
2023-11-05 09:38:58 +0100

    Linux and windows support

--- electron/scripts/bundle_python.py
+++ electron/scripts/bundle_python.py
@@ -19,22 +19,38 @@ def check_installation():
 def download_python():
     print("Detecting machine architecture...")
     arch_name = platform.machine()
+    system_name = platform.system()
 
-    if arch_name == "x86_64":
-        print("Detected Intel architecture.")
-        file_name = "cpython-3.10.13+20231002-x86_64-apple-darwin-install_only.tar.gz"
-        download_url = "https://github.com/indygreg/python-build-standalone/releases/download/20231002/" + file_name
+    file_name = None
+    download_url = None
 
-    elif arch_name == "arm64":
-        print("Detected Apple M1 architecture.")
-        file_name = "cpython-3.10.13+20231002-aarch64-apple-darwin-install_only.tar.gz"
-        download_url = "https://github.com/indygreg/python-build-standalone/releases/download/20231002/" + file_name
+    if system_name == "Darwin":
+        if arch_name == "x86_64":
+            print("Detected Intel architecture for macOS.")
+            variant = "x86_64-apple-darwin"
+        elif arch_name == "arm64":
+            print("Detected Apple M1 architecture.")
+            variant = "aarch64-apple-darwin"
+        else:
+            print(f"Unknown architecture: {arch_name}")
+            exit(1)
+
+    elif system_name == "Linux":
+        print("Detected Linux architecture.")
+        variant = "x86_64-unknown-linux-gnu"
+
+    elif system_name == "Windows":
+        print("Detected Windows architecture.")
+        variant = "x86_64-pc-windows-msvc-static"
 
     else:
-        print(f"Unknown architecture: {arch_name}")
+        print(f"Unknown operating system: {system_name}")
         exit(1)
 
-    print(f"Downloading standalone Python for {arch_name}...")
+    file_name = f"cpython-3.10.13+20231002-{variant}-install_only.tar.gz"
+    download_url = f"https://github.com/indygreg/python-build-standalone/releases/download/20231002/{file_name}"
+
+    print(f"Downloading standalone Python for {system_name} {arch_name}...")
     response = requests.get(download_url)
     with open(file_name, 'wb') as file:
         file.write(response.content)
@@ -42,6 +58,7 @@ def download_python():
     print("Extracting Python...")
     with tarfile.open(file_name) as tar:
         tar.extractall(path=".")
+
     os.remove(file_name)
 
 def install_dependencies():
