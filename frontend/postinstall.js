be6d80a7d1b1c3df02f175fdd695b2a442f9af42
Jakub Bilko <jakub.bilko@10clouds.com>
2023-11-08 18:54:45 +0800

    fix: use yarn for postinstall script

--- frontend/postinstall.js
+++ frontend/postinstall.js
@@ -2,7 +2,7 @@ const os = require('os');
 const exec = require('child_process').exec;
 
 if (os.platform() !== 'win32') {
-  exec('npm install fs-xattr@^0.4.0', (error, stdout, stderr) => {
+  exec('yarn add fs-xattr@^0.4.0', (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
