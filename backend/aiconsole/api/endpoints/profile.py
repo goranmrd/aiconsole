1827ec2112359f9c75eb7138980a30ff8cdcb3b7
Maciej Cielecki <cielecki@gmail.com>
2023-11-09 01:16:12 +0100

    Fixed: not found user profile image while project is not selected

--- backend/aiconsole/api/endpoints/profile.py
+++ backend/aiconsole/api/endpoints/profile.py
@@ -13,10 +13,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-    
+
 import logging
 from aiconsole.core.assets.asset import AssetType
 from aiconsole.core.project.paths import get_core_assets_directory, get_project_assets_directory
+from aiconsole.core.project.project import is_project_initialized
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
@@ -27,10 +28,11 @@ log = logging.getLogger(__name__)
 
 @router.get("/profile/{image}")
 async def profile_image(image: str):
-    image_path = get_project_assets_directory(AssetType.AGENT) / image
+    if is_project_initialized():
+        image_path = get_project_assets_directory(AssetType.AGENT) / image
 
-    if image_path.exists():
-        return FileResponse(str(image_path))
+        if image_path.exists():
+            return FileResponse(str(image_path))
 
     static_path = get_core_assets_directory(AssetType.AGENT) / image
     if static_path.exists():
