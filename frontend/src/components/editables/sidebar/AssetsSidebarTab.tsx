60bd23472142e70becdc56cc4f017b8c51e892bc
Maciej Topor <maciej.topor@10clouds.com>
2023-11-13 16:09:02 +0100

    fix: making only sidebar items scrollable

--- frontend/src/components/editables/sidebar/AssetsSidebarTab.tsx
+++ frontend/src/components/editables/sidebar/AssetsSidebarTab.tsx
@@ -17,16 +17,22 @@
 import { Asset, AssetType } from '@/types/editables/assetTypes';
 import SideBarItem from './SideBarItem';
 
-export const AssetsSidebarTab = ({assetType, assets}: {assetType: AssetType, assets: Asset[]}) => {
+export const AssetsSidebarTab = ({
+  assetType,
+  assets,
+}: {
+  assetType: AssetType;
+  assets: Asset[];
+}) => {
   return (
-      <div className="flex flex-col gap-[5px] pr-[15px]">
-        {assets.map((asset) => (
-          <SideBarItem
-            key={asset.id}
-            editableObjectType={assetType}
-            editableObject={asset}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col gap-[5px] pr-[20px] overflow-y-auto h-full max-h-[calc(100vh-210px)]">
+      {assets.map((asset) => (
+        <SideBarItem
+          key={asset.id}
+          editableObjectType={assetType}
+          editableObject={asset}
+        />
+      ))}
+    </div>
   );
 };
