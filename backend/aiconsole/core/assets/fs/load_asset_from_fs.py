a0beab8c1657b1901adcd88b8c7314d6544ec0e4
Maciej Cielecki <cielecki@gmail.com>
2023-11-15 16:50:24 +0100

    Added location parameter to load_asset_from_fs for easy specification of where to load from

--- backend/aiconsole/core/assets/fs/load_asset_from_fs.py
+++ backend/aiconsole/core/assets/fs/load_asset_from_fs.py
@@ -16,20 +16,19 @@
 
 import logging
 import os
+from typing import Optional
 
 import rtoml
 from aiconsole.core.assets.agents.agent import Agent
 from aiconsole.core.assets.asset import Asset, AssetLocation, AssetStatus, AssetType
-from aiconsole.core.assets.materials.material import (Material,
-                                                      MaterialContentType)
+from aiconsole.core.assets.materials.material import Material, MaterialContentType
 from aiconsole.core.gpt.consts import GPTMode
-from aiconsole.core.project.paths import (get_core_assets_directory,
-                                          get_project_assets_directory)
+from aiconsole.core.project.paths import get_core_assets_directory, get_project_assets_directory
 
 _log = logging.getLogger(__name__)
 
 
-async def load_asset_from_fs(asset_type: AssetType, asset_id: str) -> Asset:
+async def load_asset_from_fs(asset_type: AssetType, asset_id: str, location: AssetLocation | None = None) -> Asset:
     """
     Load a specific asset.
     """
@@ -37,12 +36,16 @@ async def load_asset_from_fs(asset_type: AssetType, asset_id: str) -> Asset:
     project_dir_path = get_project_assets_directory(asset_type)
     core_resource_path = get_core_assets_directory(asset_type)
 
-    if (project_dir_path / f"{asset_id}.toml").exists():
+    if (project_dir_path / f"{asset_id}.toml").exists() and (
+        location == None or location == AssetLocation.PROJECT_DIR
+    ):
         if (core_resource_path / f"{asset_id}.toml").exists():
             _log.info(f"Asset {asset_id} exists in core and project directory")
         location = AssetLocation.PROJECT_DIR
         path = project_dir_path / f"{asset_id}.toml"
-    elif (core_resource_path / f"{asset_id}.toml").exists():
+    elif (core_resource_path / f"{asset_id}.toml").exists() and (
+        location == None or location == AssetLocation.AICONSOLE_CORE
+    ):
         location = AssetLocation.AICONSOLE_CORE
         path = core_resource_path / f"{asset_id}.toml"
     else:
@@ -60,21 +63,20 @@ async def load_asset_from_fs(asset_type: AssetType, asset_id: str) -> Asset:
         "defined_in": location,
         "usage": str(tomldoc["usage"]).strip(),
         "usage_examples": tomldoc.get("usage_examples", []),
-        "default_status": AssetStatus(str(tomldoc.get("default_status", "enabled")).strip())
+        "default_status": AssetStatus(str(tomldoc.get("default_status", "enabled")).strip()),
     }
 
     if asset_type == AssetType.MATERIAL:
         material = Material(
             **params,
-            content_type=MaterialContentType(str(tomldoc["content_type"]).strip()))
+            content_type=MaterialContentType(str(tomldoc["content_type"]).strip()),
+        )
 
         if "content_static_text" in tomldoc:
-            material.content_static_text = str(
-                tomldoc["content_static_text"]).strip()
+            material.content_static_text = str(tomldoc["content_static_text"]).strip()
 
         if "content_dynamic_text" in tomldoc:
-            material.content_dynamic_text = str(
-                tomldoc["content_dynamic_text"]).strip()
+            material.content_dynamic_text = str(tomldoc["content_dynamic_text"]).strip()
 
         if "content_api" in tomldoc:
             material.content_api = str(tomldoc["content_api"]).strip()
