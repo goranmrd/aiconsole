da8691f9865ff1f2da7d46c665bf0c3d519d2830
Pawel Kopciara <pawel.kopciara@10clouds.com>
2023-11-15 11:45:22 +0100

    feat: close project icon in topbar

--- frontend/src/components/projects/ProjectTopBarElements.tsx
+++ frontend/src/components/projects/ProjectTopBarElements.tsx
@@ -21,6 +21,8 @@ import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { Button } from '@/components/common/Button';
 import { useAddMenu } from '@/utils/editables/useAddMenu';
+import { useDiscardAssetChangesStore } from '@/store/editables/asset/useDiscardAssetChangesStore';
+import { ProjectsAPI } from '@/api/api/ProjectsAPI';
 
 export function ProjectTopBarElements() {
   const projectName = useProjectStore((state) => state.projectName);
@@ -28,16 +30,31 @@ export function ProjectTopBarElements() {
   const { showContextMenu: showProjectContextMenu } = useProjectContextMenu();
   const { showContextMenu: showPlusMenu } = useAddMenu();
 
+  const { isChanged, setConfirmCallback } = useDiscardAssetChangesStore();
+
+  const handleBackToProjects = () => {
+    if (isChanged) {
+      setConfirmCallback(() => ProjectsAPI.closeProject());
+    } else {
+      ProjectsAPI.closeProject();
+    }
+  };
+
   return (
     <>
       <div className="flex text-sm gap-2 items-center pr-5">
-        <Link
-          to={`/chats/${uuidv4()}`}
-          className="h-11 text-grey-300 font-bold  text-lg uppercase text-primary hover:animate-pulse cursor-pointer flex gap-2 items-center ml-[20px]"
-          onContextMenu={showProjectContextMenu()}
-        >
-          {projectName}
-        </Link>
+        <div className='flex items-center justify-center gap-[70px]'>
+          <button className='w-12 h-12' onClick={handleBackToProjects}>
+            <img src="favicon.png" className="shadows-lg h-full w-full" alt="Logo" />
+          </button>
+          <Link
+            to={`/chats/${uuidv4()}`}
+            className="h-11 text-grey-300 font-bold  text-lg text-gray-300 hover:animate-pulse cursor-pointer flex gap-2 items-center"
+            onContextMenu={showProjectContextMenu()}
+          >
+            {projectName}
+          </Link>
+        </div>
 
         <Button small classNames="ml-10" variant="secondary" onClick={showPlusMenu()} onContextMenu={showPlusMenu()}>
           <PlusIcon />
