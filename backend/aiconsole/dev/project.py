997df4cbae0e61fe8cee07fd636d1e96704673fe
Maciej Cielecki <cielecki@gmail.com>
2023-11-17 00:33:09 +0100

    Fixes to materials

--- backend/aiconsole/dev/project.py
+++ backend/aiconsole/dev/project.py
@@ -3,7 +3,14 @@ from aiconsole.core.assets.load_all_assets import load_all_assets
 from aiconsole.core.assets.agents.agent import Agent
 from aiconsole.core.assets.asset import AssetType
 from aiconsole.core.assets.assets import Assets
+from aiconsole.core.assets.materials.material import Material
 
 
 async def get_all_agents() -> list[Agent]:
-    return cast(list[Agent], (await load_all_assets(AssetType.AGENT)).values())
+    lists = (await load_all_assets(AssetType.AGENT)).values()
+    return cast(list[Agent], (list[0] for list in lists))
+
+
+async def get_all_materials() -> list[Material]:
+    lists = (await load_all_assets(AssetType.MATERIAL)).values()
+    return cast(list[Material], (list[0] for list in lists))
