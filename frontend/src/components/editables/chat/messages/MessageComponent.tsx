1d04d1783f99f00103041cf00c0b2a4451d44d23
Maciej Cielecki <cielecki@gmail.com>
2023-11-21 14:39:12 +0100

    Better visuals when there is no text preceeding the tool calls

--- frontend/src/components/editables/chat/messages/MessageComponent.tsx
+++ frontend/src/components/editables/chat/messages/MessageComponent.tsx
@@ -40,96 +40,106 @@ export function MessageComponent({ message, group }: MessageProps) {
   const submitCommand = useChatStore((state) => state.submitCommand);
 
   return (
-    <div key={message.id} className="flex flex-col gap-2">
+    <div key={message.id}>
       <EditableContentMessage
         initialContent={message.content}
         isStreaming={message.is_streaming}
         handleAcceptedContent={handleSaveClick}
         handleRemoveClick={handleRemoveClick}
       >
-        <>
-          {group.role !== 'user' && (
-            <div className="flex-grow">
-              <div className="prose prose-stone dark:prose-invert ">
-                <ReactMarkdown
-                  components={{
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    a: ({ node, href, ...props }) => {
-                      if (href === 'command') {
-                        const command = props.children[0]?.toString();
-                        return (
-                          <a
-                            {...props}
-                            className="text-secondary hover:text-secondary-light cursor-pointer"
-                            onClick={() => {
-                              if (command) submitCommand(command);
-                            }}
-                          >
-                            {props.children}
-                          </a>
-                        );
-                      }
-                      return (
-                        <a href={href} {...props} target="_blank" rel="noopener noreferrer" className="text-primary">
-                          {props.children}
-                        </a>
-                      );
-                    },
-                    img: ({ src, ...props }) => {
-                      return (
-                        <a href={`${getBaseURL()}/image?path=${src}`} target="_blank">
-                          <img
-                            src={`${getBaseURL()}/image?path=${src}`}
-                            {...props}
-                            className=" max-w-xs rounded-md mr-5"
-                            alt={props.alt}
-                          />
-                        </a>
-                      );
-                    },
-                    code(props) {
-                      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                      const { children, className, inline, node, ...rest } = props;
-                      const match = /language-(\w+)/.exec(className || '');
-                      return !inline && match ? (
-                        <SyntaxHighlighter
-                          {...rest}
-                          style={vs2015}
-                          children={String(children).replace(/\n$/, '')}
-                          language={match[1]}
-                          PreTag="div"
-                        />
-                      ) : (
-                        <code {...rest} className={className}>
-                          {children}
-                        </code>
-                      );
-                    },
-                  }}
-                >
-                  {message.content}
-                </ReactMarkdown>
-                {message.is_streaming && !message.content && !message.tool_calls && <BlinkingCursor />}
-              </div>
-            </div>
+        <div className="flex flex-col gap-2">
+          {message.content && (
+            <>
+              {group.role !== 'user' && (
+                <div className="flex-grow">
+                  <div className="prose prose-stone dark:prose-invert ">
+                    <ReactMarkdown
+                      components={{
+                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                        a: ({ node, href, ...props }) => {
+                          if (href === 'command') {
+                            const command = props.children[0]?.toString();
+                            return (
+                              <a
+                                {...props}
+                                className="text-secondary hover:text-secondary-light cursor-pointer"
+                                onClick={() => {
+                                  if (command) submitCommand(command);
+                                }}
+                              >
+                                {props.children}
+                              </a>
+                            );
+                          }
+                          return (
+                            <a
+                              href={href}
+                              {...props}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="text-primary"
+                            >
+                              {props.children}
+                            </a>
+                          );
+                        },
+                        img: ({ src, ...props }) => {
+                          return (
+                            <a href={`${getBaseURL()}/image?path=${src}`} target="_blank">
+                              <img
+                                src={`${getBaseURL()}/image?path=${src}`}
+                                {...props}
+                                className=" max-w-xs rounded-md mr-5"
+                                alt={props.alt}
+                              />
+                            </a>
+                          );
+                        },
+                        code(props) {
+                          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                          const { children, className, inline, node, ...rest } = props;
+                          const match = /language-(\w+)/.exec(className || '');
+                          return !inline && match ? (
+                            <SyntaxHighlighter
+                              {...rest}
+                              style={vs2015}
+                              children={String(children).replace(/\n$/, '')}
+                              language={match[1]}
+                              PreTag="div"
+                            />
+                          ) : (
+                            <code {...rest} className={className}>
+                              {children}
+                            </code>
+                          );
+                        },
+                      }}
+                    >
+                      {message.content}
+                    </ReactMarkdown>
+                    {message.is_streaming && !message.content && !message.tool_calls && <BlinkingCursor />}
+                  </div>
+                </div>
+              )}
+
+              {group.role === 'user' && (
+                <div className="flex-grow">
+                  {message.content.split('\n').map((line, index) => (
+                    <span key={`line-${index}`} style={{ whiteSpace: 'pre-wrap' }}>
+                      {line}
+                      {index !== message.content.split('\n').length - 1 && <br />}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </>
           )}
 
-          {group.role === 'user' && (
-            <div className="flex-grow">
-              {message.content.split('\n').map((line, index) => (
-                <span key={`line-${index}`} style={{ whiteSpace: 'pre-wrap' }}>
-                  {line}
-                  {index !== message.content.split('\n').length - 1 && <br />}
-                </span>
-              ))}
-            </div>
-          )}
-        </>
+          {message.tool_calls.map((tool_call) => (
+            <ToolCall key={tool_call.id} group={group} tool_call={tool_call} />
+          ))}
+        </div>
       </EditableContentMessage>
-
-      {message.tool_calls.map((tool_call) => (
-        <ToolCall key={tool_call.id} group={group} tool_call={tool_call} />
-      ))}
     </div>
   );
 }
